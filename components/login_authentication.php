765b80b8a4822eb3e030cab99553a279d50ca566
Gizwiz <markus.sahrakorpi@gmail.com>
2017-02-16 18:10:17 +0000

    Add files via upload

---
+++ components/login_authentication.php
@@ -0,0 +1,38 @@
+<?php
+	
+	session_start();
+
+	if(isset($_REQUEST['submitLogin']) && !empty($_REQUEST['email']) && !empty($_REQUEST['password'])){
+		require '../database/dbconnect.php';
+		
+		$user_email = $_REQUEST['email'];
+		$user_password = $_REQUEST['password'];
+		
+		$sql = "SELECT user_email, user_password, user_username FROM user WHERE user_email='$user_email' AND user_password='$user_password'";
+		$res = $conn->query($sql);
+		
+		if ($res->num_rows > 0) {
+			// output data of each row
+			while($row = $res->fetch_assoc()) {
+				if ($row['user_email']===$user_email && $row['user_password']===$user_password);
+				$user_username = $row['user_username'];
+				$_SESSION['user_username'] = $user_username;
+				header('Location: ../application/userFrontpage.php?user='.$user_username);
+			}
+		} else {
+			echo 'Login Error';
+			$_SESSION['ERR'] = 'lOGINERROR';
+			session_write_close();
+			header("location: ../login.php");
+			exit();
+		}
+		
+	} else {
+		echo 'Login Error';
+		$SESSION['ERR'] = 'lOGINERROR';
+		session_write_close();
+		header("location: ../login.php");
+		exit();
+	}
+
+?>
