d336346ae353354ad20efe0e623de9b2241d57eb
Gizwiz <markus.sahrakorpi@gmail.com>
2017-02-17 20:39:38 +0200

    Add files via upload

--- database/professions.php
+++ database/professions.php
@@ -1,15 +1,8 @@
 <?php include 'dbConnect.php' ?>
-
+<?php include 'fieldIds.php' ?>
 <?php
 	
-	$mathematics_field_id = 1;
-	$IT_field_id = 2;
-	$engineering_field_id = 3;
-	$medical_field_id = 4;
-	$agricultural_field_id = 5;
-	$social_field_id = 6;
-	$humanities_field_id = 7;
-	$designer_field_id = 8;
+
 
 	$mathematics_professions = array(
 		
