d7a77f906cf6a82d33a6209ac5f3c7c15511d061
Gizwiz <markus.sahrakorpi@gmail.com>
2017-02-22 21:11:14 +0200

    Delete myInbox.php

--- application/myInbox.php
+++
@@ -1,9 +0,0 @@
-<?php require '../components/authentication.php' ?>
-<?php require '../components/session-check.php' ?>
-<?php include '../database/dbconnect.php' ?>
-
-<?php include '../controllers/headApplication.php' ?>
-
-<body>
-	<?php include '../controllers/navApplication.php'; ?>
-</body>
