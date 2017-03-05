a327c27842ad62d899a7c9071b06eb597c4c8f63
Gizwiz <markus.sahrakorpi@gmail.com>
2017-03-05 21:08:52 +0200

    Add files via upload

--- database/companydatabase.sql
+++ database/companydatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 02, 2017 at 10:03 PM
+-- Generation Time: Mar 05, 2017 at 08:08 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
